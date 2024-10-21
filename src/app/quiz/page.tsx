0e0169f6f7f1313a68741f54b66c18736f86a695
matthewglasser <matthewsglasser@gmail.com>
2024-10-21 10:28:04 -0400

    education question

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -251,6 +251,143 @@ const allQuestions = [
   text: string;
 }[];
 
+// {
+//    "code" : "17-2071.00",
+//    "title" : "Electrical Engineers",
+//    "tags" : {
+//       "bright_outlook" : true,
+//       "green" : false,
+//       "apprenticeship" : false
+//    },
+//    "also_called" : {
+//       "title" : [
+//          "Design Engineer",
+//          "Electrical Design Engineer",
+//          "Electrical Engineer",
+//          "Project Engineer"
+//       ]
+//    },
+//    "what_they_do" : "Research, design, develop, test, or supervise the manufacturing and installation of electrical equipment, components, or systems for commercial, industrial, military, or scientific use.",
+//    "on_the_job" : {
+//       "task" : [
+//          "Design, implement, maintain, or improve electrical instruments, equipment, facilities, components, products, or systems for commercial, industrial, or domestic purposes.",
+//          "Oversee project production efforts to assure projects are completed on time and within budget.",
+//          "Direct or coordinate manufacturing, construction, installation, maintenance, support, documentation, or testing activities to ensure compliance with specifications, codes, or customer requirements."
+//       ]
+//    },
+//    "career_video" : true,
+//    "resources" : {
+//       "resource" : [
+//          {
+//             "href" : "https://services.onetcenter.org/ws/mnm/careers/17-2071.00/knowledge",
+//             "title" : "Knowledge"
+//          },
+//          {
+//             "href" : "https://services.onetcenter.org/ws/mnm/careers/17-2071.00/skills",
+//             "title" : "Skills"
+//          },
+//          {
+//             "href" : "https://services.onetcenter.org/ws/mnm/careers/17-2071.00/abilities",
+//             "title" : "Abilities"
+//          },
+//          {
+//             "href" : "https://services.onetcenter.org/ws/mnm/careers/17-2071.00/personality",
+//             "title" : "Personality"
+//          },
+//          {
+//             "href" : "https://services.onetcenter.org/ws/mnm/careers/17-2071.00/technology",
+//             "title" : "Technology"
+//          },
+//          {
+//             "href" : "https://services.onetcenter.org/ws/mnm/careers/17-2071.00/education",
+//             "title" : "Education"
+//          },
+//          {
+//             "href" : "https://services.onetcenter.org/ws/mnm/careers/17-2071.00/job_outlook",
+//             "title" : "Job Outlook"
+//          },
+//          {
+//             "href" : "https://services.onetcenter.org/ws/mnm/careers/17-2071.00/check_out_my_state",
+//             "title" : "Check Out My State"
+//          },
+//          {
+//             "href" : "https://services.onetcenter.org/ws/mnm/careers/17-2071.00/explore_more",
+//             "title" : "Explore More"
+//          }
+//       ]
+//    }
+// }
+
+const ratings = [
+  "Strongly Dislike",
+  "Dislike",
+  "Neutral",
+  "Like",
+  "Strongly Like",
+];
+
+const trainingLevel = [
+  {
+    title: "Little or No Preparation Needed",
+    experience:
+      "Little or no previous work-related skill, knowledge, or experience is needed for these careers. For example, a person can become a waiter or waitress even if he/she has never worked before.",
+    education:
+      "Some of these careers may need a high school diploma or GED certificate.",
+    job_training:
+      "Employees in these careers need from a few days to a few months of training. Usually, an experienced worker can show you how to do the job.",
+    examples:
+      "These careers involve following instructions and helping others. Examples include agricultural equipment operators, dishwashers, floor sanders and finishers, landscaping and groundskeeping workers, logging equipment operators, baristas, and maids and housekeeping cleaners.",
+    svp_range: "(Below 4.0)",
+  },
+  {
+    title: "Some Preparation Needed",
+    experience:
+      "Some previous work-related skill, knowledge, or experience is usually needed. For example, it would help a teller to have experience working with the public.",
+    education: "These careers usually need a high school diploma.",
+    job_training:
+      "Employees in these careers need from a few months to one year of working with experienced employees. An apprenticeship program may be available for these careers.",
+    examples:
+      "These careers often involve using your knowledge and skills to help others. Examples include orderlies, counter and rental clerks, customer service representatives, security guards, upholsterers, tellers, and dental laboratory technicians.",
+    svp_range: "(4.0 to < 6.0)",
+  },
+  {
+    title: "Medium Preparation Needed",
+    experience:
+      "Previous work-related skill, knowledge, or experience is needed for these careers. For example, an electrician must be in an apprenticeship for three to four years or have several years of job training. You may need to pass a test to get a license to do the job.",
+    education:
+      "Most of these careers need vocational school training, on-the-job experience, or an associate's degree.",
+    job_training:
+      "Employees in these careers need one or two years of training. Both on-the-job experience and informal training with experienced workers may be needed. An apprenticeship program may be a good choice for these careers.",
+    examples:
+      "These careers usually involve using communication and organizational skills to coordinate, supervise, manage, or train others to accomplish goals. Examples include hydroelectric production managers, desktop publishers, electricians, agricultural technicians, barbers, court reporters and simultaneous captioners, and medical assistants.",
+    svp_range: "(6.0 to < 7.0)",
+  },
+  {
+    title: "High Preparation Needed",
+    experience:
+      "Long term work-related skill, knowledge, or experience is needed for these careers. For example, an accountant must complete four years of college and work several years in the field to be qualified for the job.",
+    education:
+      "Most of these careers need a four-year bachelor's degree, but some do not.",
+    job_training:
+      "Employees in these careers need several years of work-related experience and training. Both on-the-job and classroom job training may be needed.",
+    examples:
+      "Many of these careers involve coordinating, supervising, managing, or training others. Examples include real estate brokers, sales managers, database administrators, graphic designers, conservation scientists, art directors, and cost estimators.",
+    svp_range: "(7.0 to < 8.0)",
+  },
+  {
+    title: "Extensive Preparation Needed",
+    experience:
+      "Extensive skill, knowledge, and experience are needed for these careers. Many require more than five years of experience. For example, surgeons must complete four years of college and an additional five to seven years of specialized medical training to be able to do their job.",
+    education:
+      "Most of these careers need a graduate school education. For example, they may require a master's degree, and some require a Ph.D., M.D., or J.D. (law degree).",
+    job_training:
+      "Employees may need some on-the-job training. However, the person will usually have the needed skills, knowledge, work-related experience, and training before starting the job.",
+    examples:
+      "These careers often involve coordinating, training, supervising, or managing the activities of others to accomplish goals. Very advanced communication and organizational skills are required. Examples include pharmacists, lawyers, astronomers, biologists, clergy, physician assistants, and veterinarians.",
+    svp_range: "(8.0 and above)",
+  },
+];
+
 export default function Quiz() {
   const [results, setResults] = useState({
     Realistic: 0,
@@ -265,13 +402,9 @@ export default function Quiz() {
 
   const [selectedRatings, setSelectedRatings] = useState<number[]>([]);
 
-  const ratings = [
-    "Strongly Dislike",
-    "Dislike",
-    "Neutral",
-    "Like",
-    "Strongly Like",
-  ];
+  const [selectedTrainingLevel, setSelectedTrainingLevel] = useState<
+    number | null
+  >(null);
 
   useEffect(() => {
     const newResults = {
@@ -297,7 +430,7 @@ export default function Quiz() {
     <div className="flex flex-col items-center justify-center space-y-12 p-12">
       <h1 className="text-4xl font-bold">Quiz</h1>
       <div className="mx-auto w-full max-w-3xl space-y-6 p-6">
-        {questionIndex === -1 && (
+        {/* {questionIndex === -1 && (
           <>
             <p className="text-center text-lg font-semibold">
               Rate how much you would enjoy each of the following activities.
@@ -307,7 +440,7 @@ export default function Quiz() {
               activity or how much money it would earn.
             </p>
           </>
-        )}
+        )} */}
         {questionIndex > -1 && questionIndex < allQuestions.length && (
           <>
             <h2 className="text-center text-2xl font-bold">
@@ -363,12 +496,103 @@ export default function Quiz() {
                 </div>
               ))}
             </div>
-            <p className="text-center">
-              Move on to the next page to see your recommended careers.
-            </p>
+          </>
+        )}
+        {questionIndex === allQuestions.length + 1 && (
+          <>
+            <h2 className="text-center text-2xl font-bold">
+              How much training, education, or experience are you willing to
+              obtain?
+            </h2>
+            <div className="grid grid-cols-1 gap-4">
+              {trainingLevel.map((level, index) => (
+                <Button
+                  key={level.title}
+                  variant={
+                    selectedTrainingLevel === index ? "default" : "outline"
+                  }
+                  className="flex h-fit w-full flex-col items-start justify-start px-6 py-4"
+                  onClick={() => setSelectedTrainingLevel(index)}
+                >
+                  <span className="w-full text-left text-lg font-bold">
+                    {level.title}
+                  </span>
+                  <ul className="space-y-2 whitespace-normal text-left">
+                    <li>
+                      <span className="font-bold">Experience:</span>{" "}
+                      <span
+                        className={
+                          selectedTrainingLevel !== index
+                            ? "text-muted-foreground"
+                            : "text-gray-300"
+                        }
+                      >
+                        {level.experience}
+                      </span>
+                    </li>
+                    <li>
+                      <span className="font-bold">Education:</span>{" "}
+                      <span
+                        className={
+                          selectedTrainingLevel !== index
+                            ? "text-muted-foreground"
+                            : "text-gray-300"
+                        }
+                      >
+                        {level.education}
+                      </span>
+                    </li>
+                    <li>
+                      <span className="font-bold">Job Training:</span>{" "}
+                      <span
+                        className={
+                          selectedTrainingLevel !== index
+                            ? "text-muted-foreground"
+                            : "text-gray-300"
+                        }
+                      >
+                        {level.job_training}
+                      </span>
+                    </li>
+                    <li>
+                      <span className="font-bold">Examples:</span>{" "}
+                      <span
+                        className={
+                          selectedTrainingLevel !== index
+                            ? "text-muted-foreground"
+                            : "text-gray-300"
+                        }
+                      >
+                        {level.examples}
+                      </span>
+                    </li>
+                    <li>
+                      <span className="font-bold">SVP Range:</span>{" "}
+                      <span
+                        className={
+                          selectedTrainingLevel !== index
+                            ? "text-muted-foreground"
+                            : "text-gray-300"
+                        }
+                      >
+                        {level.svp_range}
+                      </span>
+                    </li>
+                  </ul>
+                </Button>
+              ))}
+            </div>
+            {selectedRatings[questionIndex] ? (
+              <p className="text-center text-muted-foreground">
+                You selected: {ratings[selectedRatings[questionIndex]]}
+              </p>
+            ) : (
+              <div className="h-6" />
+            )}
           </>
         )}
         <div className="flex justify-between">
+          {/* {questionIndex > -1 && ( */}
           <Button
             variant="outline"
             className="flex items-center"
@@ -377,6 +601,7 @@ export default function Quiz() {
             <ArrowLeft className="mr-2 h-4 w-4" />
             Back
           </Button>
+          {/* )} */}
           <Button
             disabled={
               selectedRatings[questionIndex] === undefined &&
