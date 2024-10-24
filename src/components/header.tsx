b82ebb83fddd3f04198088f99033d9c00582a9dd
Hiro <hryk.sakuma@gmail.com>
2024-10-24 09:36:41 -0400

    header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,38 +15,40 @@ export default function Header() {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <div className="container mx-auto px-4 py-4">
+      <div className="container mx-auto py-4">
         <div className="flex items-center">
-          <Link href="/">
-            <div className="flex items-center space-x-2">
-              <Image
-                src="/northstar.png"
-                alt="North Star Logo"
-                width={40}
-                height={40}
-                className="rounded-full"
-              />
-              <span
-                className={`text-2xl font-bold ${isHovered ? "text-black" : "text-black"}`}
-              >
-                North Star
-              </span>
-            </div>
-          </Link>
-          <nav className="flex-grow">
+          <div className="w-1/3">
+            <Link href="/">
+              <div className="flex items-center space-x-2">
+                <Image
+                  src="/northstar.png"
+                  alt="North Star Logo"
+                  width={40}
+                  height={40}
+                  className="rounded-full"
+                />
+                <span
+                  className={`text-2xl font-bold ${isHovered ? "text-black" : "text-black"}`}
+                >
+                  North Star
+                </span>
+              </div>
+            </Link>
+          </div>
+          <nav className="mx-auto flex flex-grow items-center justify-center">
             <ul className="flex justify-center space-x-6">
               <li key="questionnaire">
                 <Link
                   href="/quiz"
-                  className={`text-lg font-semibold ${isHovered ? "text-blue-900" : "text-stone-300"} duration-300 hover:text-blue-700`}
+                  className={`text-lg ${isHovered ? "font-semibold text-blue-900" : "text-stone-300"} duration-300 hover:text-blue-700`}
                 >
                   Questionnaire
                 </Link>
               </li>
-              <li key="careers">
+              <li key="careers" className="flex-grow text-center">
                 <Link
                   href="/careers"
-                  className={`text-lg font-semibold ${isHovered ? "text-blue-900" : "text-stone-300"} duration-300 hover:text-blue-700`}
+                  className={`text-lg ${isHovered ? "font-semibold text-blue-900" : "text-stone-300"} duration-300 hover:text-blue-700`}
                 >
                   Careers
                 </Link>
@@ -54,13 +56,14 @@ export default function Header() {
               <li key="courses">
                 <Link
                   href="/courses"
-                  className={`text-lg font-semibold ${isHovered ? "text-blue-900" : "text-stone-300"} duration-300 hover:text-blue-700`}
+                  className={`text-lg ${isHovered ? "font-semibold text-blue-900" : "text-stone-300"} duration-300 hover:text-blue-700`}
                 >
                   Courses
                 </Link>
               </li>
             </ul>
           </nav>
+          <div className="w-1/3"></div>
         </div>
       </div>
     </header>
